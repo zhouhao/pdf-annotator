96fc3864d09d313b02a5375686b32d54d10592fd
SaltyEgg <hzhou.me@saltyee.com>
2025-06-21 15:17:03 -0700

    update PDF.js version and add necessary CSS imports for text and annotation layers

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
+import 'react-pdf/dist/esm/Page/TextLayer.css'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 import './index.css'
 import App from './App.tsx'
 
